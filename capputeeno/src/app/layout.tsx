1d580e40b35163c0ea9862f62204b922436df44f
kevinwduran <kevinwduran23@gmail.com>
2024-09-18 16:06:54 -0300

    refactor: extract providers

--- capputeeno/src/app/layout.tsx
+++ capputeeno/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import {Saira} from 'next/font/google'
 import "./globals.css";
 import { Header } from "./components/Header";
-import { FilterContextProvider } from "@/contexts/FilterContext";
+import { DefaultProviders } from "./components/DefaultProviders";
 
 const saira = Saira({
   weight: ['300', '400', '500', '600'],
@@ -22,10 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={saira.className}>
-        <FilterContextProvider>
+        <DefaultProviders>
           <Header/>
           {children}  
-        </FilterContextProvider>    
+        </DefaultProviders>    
       </body>
     </html>
   );
